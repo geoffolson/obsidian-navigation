4fef6a523a96f5fc86e7b8a49e4e0fe14811d7d4
Manuel Odendahl <wesen@ruinwesen.com>
2024-09-14 12:07:44 -0400

    Remove the changelog debugging code

--- src/fileWatcher.ts
+++ src/fileWatcher.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import { updateFileCache } from "./fileCache";
-import { updateChangelog } from "./utils";
 
 export function setupFileWatcher(fileCache: Map<string, string[]>): vscode.FileSystemWatcher {
   const watcher = vscode.workspace.createFileSystemWatcher("**/*.md");
@@ -10,14 +9,12 @@ export function setupFileWatcher(fileCache: Map<string, string[]>): vscode.FileS
     const relativePath = path.relative(vscode.workspace.workspaceFolders![0].uri.fsPath, uri.fsPath);
     const fileName = path.basename(uri.fsPath, ".md");
     updateFileCache(fileName.toLowerCase(), relativePath);
-    updateChangelog(`Added new file to cache: ${relativePath}`);
   });
 
   watcher.onDidDelete((uri) => {
     const relativePath = path.relative(vscode.workspace.workspaceFolders![0].uri.fsPath, uri.fsPath);
     const fileName = path.basename(uri.fsPath, ".md");
     updateFileCache(fileName.toLowerCase(), relativePath, true);
-    updateChangelog(`Removed file from cache: ${relativePath}`);
   });
 
   return watcher;
