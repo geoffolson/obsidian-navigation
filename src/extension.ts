28d89abc632cbd7a6a0151692cf501c47256045b
geoffolson <geoffrey.olson.jr@gmail.com>
2024-09-17 19:07:44 -0700

    minor refactor

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,21 @@ export function activate(context: vscode.ExtensionContext) {
   const hoverProvider = vscode.languages.registerHoverProvider("markdown", {
     async provideHover(document, position, token) {
       const range = document.getWordRangeAtPosition(position, /\[\[(.*?)\]\]/);
-      if (range) {
-        const fileName = document
-          .getText(range)
-          .replace(/\[\[|\]\]/g, "")
-          .trim();
-        const uri = (await vscode.workspace.findFiles(`**/${fileName}.md`))[0];
-        if (uri) {
-          const markdownString = new vscode.MarkdownString(
-            `[Go to ${fileName}](${uri})`
-          );
-          markdownString.isTrusted = true; // Allow the link to be clickable
-          return new vscode.Hover(markdownString, range);
-        } else {
-          return new vscode.Hover(`File ${fileName}.md not found.`);
-        }
-      }
+      if (!range) return;
+
+      const fileName = document
+        .getText(range)
+        .replace(/\[\[|\]\]/g, "")
+        .trim();
+
+      const uri = (await vscode.workspace.findFiles(`**/${fileName}.md`))[0];
+      if (!uri) return new vscode.Hover(`File ${fileName}.md not found.`);
+
+      const markdownString = new vscode.MarkdownString(
+        `[Go to ${fileName}](${uri})`
+      );
+      markdownString.isTrusted = true; // Allow the link to be clickable
+      return new vscode.Hover(markdownString, range);
     },
   });
 
